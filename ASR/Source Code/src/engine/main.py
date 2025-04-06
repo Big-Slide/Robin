2e803acebfcb649bb3eba83eda038f0eafad4849
Seyed Faridoddin Kiaei <sfdkiaei@gmail.com>
2025-04-06 16:45:19 +0330

    chore: update main with new standards

--- ASR/Source Code/src/engine/main.py
+++ ASR/Source Code/src/engine/main.py
@@ -1,66 +1,68 @@
-from fastapi import FastAPI, UploadFile
-from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
-import torch
-import librosa
-import uvicorn
-import time
-from functools import wraps
 import os
+from loguru import logger
+from version import __version__
+from config.config_handler import config
+import sys
 
-# facebook/wav2vec2-large-xlsr-53
-# openai/whisper-large-v3-turbo
-# openai/whisper-tiny
-# m3hrdadfi/wav2vec2-large-xlsr-persian-v3
-# SLPL/Sharif-wav2vec2
-model_path = '../Models/SLPL/Sharif-wav2vec2'
+if os.environ.get("MODE", "dev") == "prod":
+    log_dir = "/approot/data"
+else:
+    log_dir = "../Outputs/result"
+os.makedirs(log_dir, exist_ok=True)
 
-app = FastAPI()
+logger.remove()
+logger.add(
+    sys.stderr,
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message} | {extra}",
+    level=config["CONSOLE_LOG_LEVEL"],
+    backtrace=True,
+    diagnose=True,
+    colorize=True,
+    serialize=False,
+    enqueue=True,
+)
+logger.add(
+    f"{log_dir}/engine.log",
+    rotation="50MB",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message} | {extra}",
+    level=config["FILE_LOG_LEVEL"],
+    backtrace=True,
+    diagnose=False,
+    colorize=True,
+    serialize=False,
+    enqueue=True,
+)
 
-# Check if GPU is available and set device
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f'Device: {device}')
+logger.info("Starting service...", version=__version__)
 
-# Load model and processor
-processor = Wav2Vec2Processor.from_pretrained(model_path)
-model = Wav2Vec2ForCTC.from_pretrained(model_path).to(device)
-model.eval()  # Set model to evaluation mode
+from generators import ASRGenerator
+import aio_pika
+import asyncio
+from core.queue_utils import process_message
 
-def timer(argument = None):
-    def decorator(function):
-        @wraps(function)
-        async def wrapper(*args, **kwargs):
-            # something_with_argument(argument)
-            time_start = time.perf_counter()
-            retval = await function(*args, **kwargs)
-            print(f"Execution time: {round(time.perf_counter() - time_start, 3)} sec")
-            return retval
-        return wrapper
-    return decorator
 
-@app.post("/transcribe/")
-@timer()
-async def transcribe(audio_file: UploadFile):
-    # Save uploaded file
-    audio_path = f"temp_{audio_file.filename}"
-    with open(audio_path, "wb") as f:
-        f.write(await audio_file.read())
+async def main():
+    asr_generator = ASRGenerator()
+    connection = await aio_pika.connect_robust(config["QUEUE_CONNECTION"])
 
-    # Load audio file
-    audio, sr = librosa.load(audio_path, sr=16000)
-    input_values = processor(audio, sampling_rate=sr, return_tensors="pt", padding=True).input_values.to(device)
+    # Create separate channels for consuming and publishing
+    task_channel = await connection.channel()
+    result_channel = await connection.channel()
 
-    # Perform transcription
-    with torch.no_grad():
-        logits = model(input_values).logits
-    predicted_ids = torch.argmax(logits, dim=-1)
-    transcription = processor.batch_decode(predicted_ids)[0]
+    # Configure task queue
+    task_queue = await task_channel.declare_queue("task_queue", durable=True)
 
-    if os.path.exists(audio_path):
-        os.remove(audio_path)
+    # Start consuming tasks
+    await task_queue.consume(
+        lambda message: process_message(message, result_channel, asr_generator)
+    )
 
-    print(transcription)
-    return {"transcription": transcription}
+    # Keep connection alive
+    try:
+        await asyncio.Future()
+    finally:
+        await connection.close()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    asyncio.run(main())
