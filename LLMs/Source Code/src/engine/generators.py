d44d756cc3cf67e22d4eebae12c31e606767c852
Seyed Faridoddin Kiaei <sfdkiaei@gmail.com>
2025-06-01 17:20:45 +0330

    chore: increase token window in llm

--- LLMs/Source Code/src/engine/generators.py
+++ LLMs/Source Code/src/engine/generators.py
@@ -32,6 +32,7 @@ class LLMGenerator:
             temperature=config.MODEL_TEMPERATURE,
             num_predict=config.MODEL_NUM_PREDICT,
             top_p=config.MODEL_TOP_P,
+            num_ctx=131072,
         )
         self.cv_generator = CVGenerator(self.llm)
         self._current_model = id_model
